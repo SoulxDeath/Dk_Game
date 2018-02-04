f5f6871545c8ca4be757e0ff81c43a72c4630e13
SoulxDeath <HFairbanks@users.noreply.github.com>
2018-02-04 16:56:13 -0500

    Added Initial Project

---
+++ Donkey Kong/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey ("w"))
+		{
+			moveUp ();
+		}
+
+		if(Input.GetKey ("s"))
+		{
+			moveDown ();
+		}
+
+		if(Input.GetKey ("a"))
+		{
+			moveLeft ();
+		}
+
+		if(Input.GetKey ("d"))
+		{
+			moveRight ();
+		}
+	}
+
+	void moveUp()
+	{
+		transform.position += new Vector3 (0, moveSpeed);
+	}
+	void moveDown()
+	{
+		transform.position -= new Vector3 (0, moveSpeed);
+	}
+	void moveLeft()
+	{
+		transform.position -= new Vector3 (moveSpeed, 0);
+	}
+	void moveRight()
+	{
+		transform.position += new Vector3 (moveSpeed, 0);
+	}
+}
